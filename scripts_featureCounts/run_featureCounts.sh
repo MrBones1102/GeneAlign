0220615b6155dd4094e8afb95eece8359db15be8
MrBones1102 <jamespopoli@gmail.com>
2024-09-17 19:55:22 -0700

    discovered "salmon" as a tool to go directly from fastq.gz files to gene_counts. I'm so upset I wasted so much time :(

---
+++ scripts_featureCounts/run_featureCounts.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+#threads to use for processing
+THREADS=10
+
+#start conda environment
+conda activate rnaseq
+
+#navigate to sra_out to prefetch
+cd ./sra_out
+
+#ask for single or paired end reads
+echo "Is this [single] end or [paired] end data?"
+read endType
+if [ "$endType" = "single" ]; then
+	echo "selected [single]"
+elif [ "$endType" = "paired" ]; then
+	echo "selected [paired]"
+else
+	echo "input either [single] or [paired]"
+	exit 1
+fi
+
+#loop through accession list, fetch samples and fastq format them
+while IFS=read -r line; do
+	prefetch $line
+	if [ "$endType" = "single" ]; then
+		fasterq-dump $line
+	elif [ "$endType" = "paired" ]; then
+		fasterq-dump $line --split-files --skip-technical
+	fi
+done < accession_list.txt
+
+#navigate back to the main directory
+cd ..
+
+##perform quality control on each sample
+#fastqc -t $THREADS -o ./qc_files ./sra_out/*.fastq
+#mulitqc ./qc_files/ -o ./qc_files/
+
+##perform rRNA removal with sortmerna <SKIPPING FOR NOW>
+#<sortmerna stuff here>
+
+##perform adapter trimming with trimmomatic <SKIPPING FOR NOW>
+#<trimmomatic stuff here>
+
+#perform alignment using hisat2 and output sam files, checking for single or paired end reads
+#if single end reads, simply loop through the files
+#if paired end reads, loop through only the first file and use the second file as the second input
+if [ "$endType" = "single" ]; then
+	for file in ./sra_out/*.fastq; do
+		hisat2 -p $THREADS -x ./hisat_ref/grch38/genome -U "$file" -S ./sam_file/"$(basename "$file" .fastq).sam" --new-summary 2> ./sam_file/"$(basename "$file" .fastq)_new-summary.txt"
+	done
+elif [ "$endType" = "paired" ]; then
+	for file in ./sra_out/*_1.fastq; do
+		base=$(basename "$file" _1.fastq)
+		hisat2 -p $THREADS -x ./hisat_ref/grch38/genome -1 ./sra_out/"${base}_1.fastq" -2 ./sra_out/"${base}_2.fastq" -S ./sam_file/"${base}.sam" --new-summary 2> ./sam_file/"${base}_new-summary.txt"
+	done
+fi
+
+#convert sam files to bam files and deposit in bam_files
+for file in ./sam_file/*.sam; do
+	samtools view -@ $THREADS -bS "$file" -o ./bam_file/"$(basename "$file" .sam).bam"
+done
+
+#sort bam files and deposit in sorted_bam_files
+for file in ./bam_file/*.bam; do
+	samtools sort -@ $THREADS "$file" -o ./sorted_bam_files/"$(basename "$file" .bam)_sorted.bam"
+done
+
+##count reads using featureCounts for all sorted bam files using the gtf annotation file
+#if [ "$endType" = "single" ]; then
+#	for file in ./sorted_bam_files/*_sorted.bam; do
+#		featureCounts -T $THREADS -a ./hisat_ref/Homo_sapiens.GRCh38.112.gtf -o ./gene_counts/gene_counts.txt $file
+#	done
+#fi
+#if [ "$endType" = "paired" ]; then
+#	for file in ./sorted_bam_files/*_sorted.bam; do
+#		featureCounts -T $THREADS -a ./hisat_ref/Homo_sapiens.GRCh38.112.gtf -o ./gene_counts/gene_counts.txt
+#	done
+#fi
---
+++ scripts_featureCounts/setup_featureCounts.sh
@@ -0,0 +1,57 @@
+#make a conda install folder, download the setup shell script, run the script, remove the script
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+
+#create a new environment for RNAseq analysis
+canda create --name rnaseq
+
+#activate the newly created environment
+conda activate rnaseq
+
+#add the required channels for packages, as required by bioconda
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+
+#add the superset of tools (we may only use a subset)
+conda install fastqc
+conda install multiqc
+conda install sortmerna=4.3.6
+conda install trimmomatic
+conda install hisat2
+conda install subread
+conda install bioconductor-edger
+conda install sra-tools
+conda install samtools
+
+#setup SRA-Tool's working directory
+mkdir sra_out
+cd ./sra_out
+vdb-config --prefetch-to-cwd
+cd ..
+
+#set up quality control dirs
+mkdir qc_files
+
+#set up hisat reference genome and annotations
+mkdir hisat_ref
+cd ./hisat_ref
+wget https://genome-idx.s3.amazonaws.com/hisat/grch38_genome.tar.gz
+tar -xvzf grch38_genome.tar.gz
+wget https://ftp.ensembl.org/pub/release-112/gtf/homo_sapiens/Homo_sapiens.GRCh38.112.gtf.gz
+gzip -d Homo_sapiens.GRCh38.112.gtf.gz
+cd ..
+
+#set up sam dir as hisat2 output and input for bam conversion
+mkdir sam_files
+
+#set up bam dir as bam conversion output
+mkdir bam_files
+
+#set up sorted bam dir as sorted bam conversion output
+mkdir sorted_bam_files
+
+#set up feature counts dir
+mkdir gene_counts
